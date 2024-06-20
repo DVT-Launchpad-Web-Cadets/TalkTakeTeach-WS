d48aebd0bf552fd44b32e7bf52ea4f0f82d0cc03
Ayaaz Street <astreet@dvtsoftware.com>
2024-06-20 20:18:22 +0200

    🏷️ Changed types from string to integer, added number of reviews to model

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { chromium } from "playwright";
 import ProductInterface from "./models/models";
 
 (async () => {
+	console.log("\nSetting up web scraper\n");
 	const scrapedProducts: ProductInterface[] = [];
 	const browser = await chromium.launch();
 	const context = await browser.newContext();
 	const page = await context.newPage();
+	console.log(
+		"\nStarting to scrape https://www.takealot.com/all?sort=Relevance%EF%BB%BF%EF%BB%BF\n"
+	);
 	const url = "https://www.takealot.com/all?sort=Relevance%EF%BB%BF%EF%BB%BF";
 	try {
 		await page.goto(url);
@@ -31,23 +35,26 @@ import ProductInterface from "./models/models";
 			const productLink =
 				"https://www.takealot.com" +
 				(await product.locator("> a").getAttribute("href"));
-			const rating = await product
-				.locator("div.rating > div")
-				.first()
-				.innerText();
-			const price = await product
-				.locator("div.card-section ul li")
-				.last()
-				.innerText();
+			const price = parseInt(
+				(await product.locator("div.card-section ul li").last().innerText())
+					.split(" ")
+					.reverse()[0]
+			);
 
 			const productObject: ProductInterface = {
 				name,
 				imageUrl: imageUrl ?? "",
 				productLink,
-				rating,
 				price,
 			};
-
+			const hasSale = await product.locator("div.card-section ul li").count();
+			if (hasSale > 1) {
+				productObject.salePrice = parseInt(
+					(await product.locator("div.card-section ul li").first().innerText())
+						.split(" ")
+						.reverse()[0]
+				);
+			}
 			const hasBrand = await product.locator("a > span").count();
 			if (hasBrand >= 1) {
 				productObject.brand = await product.locator("a > span").innerText();
@@ -58,22 +65,27 @@ import ProductInterface from "./models/models";
 						.locator("..")
 						.getAttribute("href"));
 			}
-
-			const hasSale = await product.locator("div.card-section ul li").count();
-			if (hasSale > 1) {
-				productObject.salePrice = await product
-					.locator("div.card-section ul li")
-					.first()
-					.innerText();
+			const hasRating = await product.locator("div.rating").count();
+			if (hasRating >= 1) {
+				const ratingAndReviews = (
+					await product.locator("div.rating > div").first().innerText()
+				)
+					.trim()
+					.split("(");
+				productObject.rating = parseFloat(ratingAndReviews[0].slice(0, -1));
+				productObject.numberOfReviews = parseFloat(
+					ratingAndReviews[1].slice(0, -1)
+				);
 			}
 
 			scrapedProducts.push(productObject);
 		}
 		console.log("Products:", scrapedProducts);
-		console.log("Number of products:", scrapedProducts.length);
+		console.log("Number of products scraped:", scrapedProducts.length);
 	} catch (error) {
 		console.error("Error while scraping:", error);
 	} finally {
 		await browser.close();
+		console.log("\nWeb scraping ended X_X\n");
 	}
 })();
--- src/models/models.ts
+++ src/models/models.ts
@@ -1,10 +1,11 @@
 export default interface ProductInterface {
-    name: string;
-    imageUrl: string;
-    brand?: string;
-    brandLink?: string;
-    productLink: string;
-    rating: string;
-    price: string;
-    salePrice?: string;
+	name: string;
+	imageUrl: string;
+	brand?: string;
+	brandLink?: string;
+	productLink: string;
+	rating?: number;
+	numberOfReviews?: number;
+	price: number;
+	salePrice?: number;
 }
