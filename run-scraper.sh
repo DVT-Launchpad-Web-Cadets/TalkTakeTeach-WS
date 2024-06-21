ef58d7280bac8a02c644f7f26362e2a8720d322b
Megan Hugo <mhugo@dvtsoftware.com>
2024-06-21 20:15:06 +0200

    feat: :sparkles: Added Delete Functionality

--- run-scraper.sh
+++ run-scraper.sh
@@ -1,2 +1,9 @@
 cd /app
-/usr/local/bin/bun run src/index.ts
+
+if [ "$1" = "job1" ]; then
+    /usr/local/bin/bun run src/index.ts
+elif [ "$1" = "job2" ]; then
+    /usr/local/bin/bun run src/delete.ts
+else
+    echo "Unknown job: $1"
+fi
